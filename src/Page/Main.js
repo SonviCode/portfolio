5ce611e767b2fd37fdde08c73b5ea33550b18429
Tom Sonvico <tom.sonvico.pro@gmail.com>
2022-10-12 12:12:31 +0200

    responsiv

--- src/Page/Main.js
+++ src/Page/Main.js
@@ -18,7 +18,7 @@ export default function Main() {
 
   return (
     <div
-      className=" after:bg-[#000] after:content-[''] after:w-1/2 after:h-full after:fixed after:top-0 after:z-40
+      className="w-full after:bg-[#000] after:content-[''] after:w-1/2 after:h-full after:fixed after:top-0 after:z-40
     before:bg-[#000] before:content-[''] before:w-1/2 before:h-full before:fixed before:top-0 before:right-0 before:z-40
     before:animate-[apparitionOnRight_2s_ease-in-out_forwards]
     after:animate-[apparitionOnLeft_2s_ease-in-out_forwards]
