2093ebed585ec57a07c29297ccb98e204603f5f9
Rishikesh S <rkesh2003@gmail.com>
2025-02-05 09:40:21 +0530

    chore: data setup for projects, exp, socials

---
+++ data/projects.ts
@@ -0,0 +1,10 @@
+export interface Project {
+  name: string;
+  description: string;
+  projectWebsite: string;
+  projectThumbnail: string;
+  projectGithub: string;
+  skills: string[];
+}
+
+export const projects: Project[] = [];
---
+++ data/socials.ts
@@ -0,0 +1,7 @@
+export interface Social {
+  name: string;
+  link: string;
+  icon: string;
+}
+
+export const socials: Social[] = [];
